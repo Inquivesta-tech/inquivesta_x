fe03ba95eb334e72993d8d0f9acfa1db42548c3a
Amardeep <amarthescientist@gmail.com>
2023-03-29 07:27:27 +0530

    .

--- contact.js
+++ contact.js
@@ -2,6 +2,6 @@ function submit() {
     conatactName = document.getElementById("fname").value
     contactNo = document.getElementById("phone").value
     query = document.getElementById("Query").value
-    
-    window.location.href = `mailto:inquivesta@iiserkol.ac.in?subject=Website query&body=Name ${conatactName} %3A%0D%0A Phone ${contactNo} %3A%0D%0A Query ${query}`
+
+    window.location.href = `mailto:inquivesta@iiserkol.ac.in?subject=Website query&body=Name %3A ${conatactName} %0D%0A Phone %3A ${contactNo} %0D%0A Query %3A ${query}`
 }
